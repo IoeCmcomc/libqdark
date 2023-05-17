885ef11cb879c1d01578753840cca9da3f2b7a86
IoeCmcomc <53734763+IoeCmcomc@users.noreply.github.com>
2023-05-17 12:58:25 +0700

    bugfix: Fix error "No Target Architecture" when compling with MSVC

--- src/windows10themenotifier.h
+++ src/windows10themenotifier.h
@@ -4,38 +4,37 @@
 #include <memory>
 
 #include <QObject>
+#include <Windows.h> // This fixes error "No Target Architecture" when compling with MSVC
 #include <windef.h>
 
 class QWinEventNotifier;
 
 namespace libqdark {
-
-class Windows10ThemeNotifier final: public QObject
-{
-    Q_OBJECT
+    class Windows10ThemeNotifier final : public QObject
+    {
+        Q_OBJECT
 public:
-    explicit Windows10ThemeNotifier(QObject *parent = nullptr);
+        explicit Windows10ThemeNotifier(QObject *parent = nullptr);
 
-    virtual ~Windows10ThemeNotifier() override;
+        virtual ~Windows10ThemeNotifier() override;
 
-    void open();
-    void close();
-    void setEnabled(bool enabled);
+        void open();
+        void close();
+        void setEnabled(bool enabled);
 
-    void checkCurrentThemeAndNotify();
+        void checkCurrentThemeAndNotify();
 
 public slots:
-   void handler();
+        void handler();
 
 signals:
-   void signalLightTheme();
-   void signalDarkTheme();
+        void signalLightTheme();
+        void signalDarkTheme();
 
 private:
-    std::unique_ptr<QWinEventNotifier> m_winEventNotifier;
-    HANDLE m_eventHandle;
-    HKEY   m_hKey;
-    std::atomic_bool m_isOpened;
-};
-
+        std::unique_ptr<QWinEventNotifier> m_winEventNotifier;
+        HANDLE m_eventHandle;
+        HKEY m_hKey;
+        std::atomic_bool m_isOpened;
+    };
 } // namespace libqdark
